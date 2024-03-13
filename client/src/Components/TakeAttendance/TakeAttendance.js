229fa8133930138919e11d078f722f7c0fca3eae
salman karim <124361112+Salman-Khan9@users.noreply.github.com>
2024-03-13 15:25:27 +0500

    add to class button
    add to class button

--- client/src/Components/TakeAttendance/TakeAttendance.js
+++ client/src/Components/TakeAttendance/TakeAttendance.js
@@ -86,7 +86,7 @@ const TakeAttendance = () => {
 
   return (
     <>
-    (
+    
         <div className='class-buttons'>
           <span className='fw-bold fs-3'>Select class:</span>
           {classname.map((classes, index) => (
@@ -99,15 +99,15 @@ const TakeAttendance = () => {
             </button>
           ))}
         </div>
-      )
-      {createclassbutton ? null : (
+      
+      {createclassbutton ? null : 
         <div className='create-class-button'>
           <Link className='create-class-link' to='/add/students/in/class'>
             Add Students in Class
             <MdOutlinePersonAdd size={22} className='icon' />
           </Link>
         </div>
-      )}
+      }
       {filteredstudents.length>0 ? (
         <div className='attendance-container'>
           <div className='students-list'>
