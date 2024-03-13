736ce3cb5bed7e58626cb50be090017235fa327b
salman karim <124361112+Salman-Khan9@users.noreply.github.com>
2024-03-13 18:08:40 +0500

    routes
    routes

--- client/src/App.js
+++ client/src/App.js
@@ -8,31 +8,19 @@ import CreateClass from './Pages/CreateClass/CreateClass';
 import AttendanceHistory from './Pages/AttendanceHistory/AttendanceHistory';
 import { ToastContainer } from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css';
-import { useSelector } from 'react-redux';
-import { selectloggedstatus } from './Redux/Slices/AuthSlice';
 
 function App() {
-  const logged = useSelector(selectloggedstatus)
   return (
     
     <Router>
       <ToastContainer/>
-      
-        {logged?
-        <Routes>
+      <Routes>
       <Route path='/' element={<Home/>}/>
-      <Route path='/Attendance/History' element={<AttendanceHistory/>}/>
-        <Route path='/add/students/in/class' element={<CreateClass/>}/>
-        </Routes>
-        :
-        <Routes>
         <Route path='/login' element={<Login/>}/>
         <Route path='/signup' element={<Signup/>}/>
+        <Route path='/Attendance/History' element={<AttendanceHistory/>}/>
+        <Route path='/add/students/in/class' element={<CreateClass/>}/>
       </Routes>
-        }
-       
-        
-      
     </Router>
   );
 }
